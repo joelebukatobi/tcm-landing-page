3a347754bd6dbf31e1d7a394b99a016600452af8
joelebukatobi <joelebuka@gmail.com>
2024-05-14 00:03:06 +0100

    Made styling changes to the navbar page for mobile

--- src/app/_components/organisms/Navbar.jsx
+++ src/app/_components/organisms/Navbar.jsx
@@ -1,3 +1,7 @@
+'use client';
+// React
+import { useState } from 'react';
+
 // Next JS
 import Image from 'next/image';
 
@@ -7,34 +11,35 @@ import { Container } from '@/_components/molecules/Container';
 // Images
 import Logo from '@/_assets/images/icons/logo.svg';
 import IconMenu from '@/_assets/images/icons/icon-menu.svg';
+import IconClose from '@/_assets/images/icons/icon-close.svg';
 import Link from 'next/link';
 
 export const Navbar = () => {
+  const [open, setOpen] = useState();
   return (
-    <Container>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <Image className="d-block d-lg-none" src={Logo} alt="" />
-          <h4>The Campus Manager</h4>
-        </div>
-        <div className="navbar-menu">
-          <Image className="d-block d-lg-none" src={IconMenu} alt="" />
-        </div>
-        <ul className="navbar-link">
-          <li className="navbar-item">
-            <Link href="#">Home</Link>
-          </li>
-          <li className="navbar-item">
-            <Link href="#">Product</Link>
-          </li>
-          <li className="navbar-item">
-            <Link href="#">About</Link>
-          </li>
-          <li className="navbar-item">
-            <Link href="#">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </Container>
+    <nav className={open ? `navbar fixed w-full h-full items-start` : `navbar relative items-center`}>
+      <div className="navbar-logo">
+        <Image className="d-block d-lg-none" src={Logo} alt="" />
+        <h4>The Campus Manager</h4>
+      </div>
+      <div className="navbar-menu" onClick={() => setOpen(!open)}>
+        <Image className="d-block d-lg-none" src={open ? IconClose : IconMenu} alt="" />
+      </div>
+
+      <ul className={open ? `navbar-link flex` : `navbar-link hidden lg:flex`}>
+        <li className="navbar-item">
+          <Link href="#">Home</Link>
+        </li>
+        <li className="navbar-item">
+          <Link href="#">Product</Link>
+        </li>
+        <li className="navbar-item">
+          <Link href="#">About</Link>
+        </li>
+        <li className="navbar-item">
+          <Link href="#">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
