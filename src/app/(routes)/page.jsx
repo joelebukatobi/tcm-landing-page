a0920e36cc6390c533620dc5e6ff2dc890df55ee
joelebukatobi <joelebuka@gmail.com>
2024-05-15 08:45:04 +0100

    Added the modal to the index page

--- src/app/(routes)/page.jsx
+++ src/app/(routes)/page.jsx
@@ -1,32 +1,87 @@
+'use client';
+// React
+import { useState, useEffect } from 'react';
+
 // Next JS
 import Image from 'next/image';
+
+// Config
+import { AIRTABLE_TOKEN } from '@/_config/index';
 
 // COmponents
 import { Button } from '@/_components/atoms/Button';
 import { Input } from '@/_components/atoms/Input';
 import { Container } from '@/_components/molecules/Container';
+import { Modal } from '@/_components/organisms/Modal';
 
 // Image
 import Screens from '@/_assets/images/pictures/screens.png';
 export default function Home() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [modal, setModal] = useState('');
+
+  // HandleSubmit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch(`https://api.airtable.com/v0/appPM2FECWlBlA4re/registrations`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ fields: { name, email } }),
+    });
+
+    const data = await res.json();
+    console.log(data, res.status);
+
+    if (res.status === 200) {
+      setTimeout(() => {
+        setModal(true);
+      }, 3000); // 3 seconds
+    }
+  };
+
   return (
-    <div className="landing">
-      <Container>
-        <header>
-          <h1>We are Strengthening Parent-School Partnerships.</h1>
+    <>
+      <section className="landing">
+        <Container>
+          <header>
+            <h1>We are Strengthening Parent-School Partnerships.</h1>
 
-          <p>Be First In Line When We Launch</p>
-        </header>
+            <p>Be First In Line When We Launch</p>
+          </header>
 
-        <form action="">
-          <Button>Sign Up</Button>
-          <Input placeholder="Enter email" type="email" name="email" id="email" />
-          <Input placeholder="Name" type="text" name="name" id="name" />
-        </form>
-      </Container>
-      <div className="landing-image">
-        <Image src={Screens} />
-      </div>
-    </div>
+          <form action="" onSubmit={handleSubmit}>
+            <Button>Sign Up</Button>
+            <Input
+              placeholder="Enter email"
+              type="email"
+              name="email"
+              id="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="Name"
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </form>
+        </Container>
+        <div className="landing-image">
+          <Image src={Screens} />
+        </div>
+      </section>
+      <section className={modal ? 'modal flex' : 'hidden'}>
+        <Modal modal={modal} />
+      </section>
+    </>
   );
 }
